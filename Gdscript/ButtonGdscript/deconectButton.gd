550ca4525f402d19755cb1a0f4903aafd24fdffd
Samuel <sassani134@gmail.com>
2021-04-30 09:36:43 +0200

    Deconnexion button is valid

--- Gdscript/ButtonGdscript/deconectButton.gd
+++ Gdscript/ButtonGdscript/deconectButton.gd
@@ -8,7 +8,9 @@ extends Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$HTTPRequest.connect("request_completed", self, "_on_request_completed")
+	print("OK done")
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,9 +19,15 @@ func _ready():
 
 
 func _on_HTTPRequest_request_completed(result, response_code, headers, body):
-	
-	pass # Replace with function body.
+	var response = parse_json(body.get_string_from_utf8())
+	print(headers)
+	print(response_code)
+	if response_code == 200:
+		get_tree().change_scene("res://Scenes/Login.tscn")
 
 
 func _on_deconectButton_pressed():
-	pass # Replace with function body.
+	var headers = ["Content-Type: application/json","uid:" +Global.uid,"client:" +Global.client, "access-token:"+ Global.accessToken]
+	var error = $HTTPRequest.request("http://127.0.0.1:3000/auth/sign_out",headers, true, HTTPClient.METHOD_DELETE)
+	if error != OK:
+		push_error(" An error occured in  the HTTP request")
